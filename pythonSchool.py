92541192fcc8a8c80fd6c542f6f7a3ed1cfdff48
Grzegorz Guzniczak School <GresorerBiz@protonmail.com>
2020-10-06 09:04:18 +0200

    school 06.10.2020

---
+++ pythonSchool.py
@@ -0,0 +1,28 @@
+# i = 1
+# while i < 6:
+#     print(i)
+#     if i == 3:
+#         break
+#     i+=1
+# m = 0
+# while m<1 or m>12:
+#     m = int(input('Podaj miesiac:'))
+# a = int(input('Podaj a:'))
+# b = int(input('Podaj b:'))
+
+# while a != b:
+#     if a > b:
+#         a -= b
+#     else:
+#         b -= a
+# print('NWD: '+ str(a))
+
+# a = int(input('Podaj a:'))
+# b = int(input('Podaj b:'))
+# i = 1
+# while b != 0:
+#     r = a%b
+#     a = b
+#     b = r
+#     i += 1
+# print('NWD: ' + str(a) + str(i))
